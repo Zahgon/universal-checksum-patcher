dc0b41120b05baa072262532612c21eb60f92f2d
Illia Yalovoi <imperio091@gmail.com>
2026-07-01 21:20:39 +0000

    remove old byte-signature patcher

--- error.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "errors"
-
-var (
-	errNoMatch    = errors.New("cannot detect bytes pattern to patch. Most likely patcher are outdated due to game updates or pather already was applied")
-	errCantLocate = errors.New("cannot locate file in current directory")
-)
